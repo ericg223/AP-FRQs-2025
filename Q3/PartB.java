753be5e760ee42abe5649ee76d19041d92b5021b
ericg223 <ericg223@nycstudents.net>
2025-05-21 22:17:12 -0400

    Update PartB.java

--- Q3/PartB.java
+++ Q3/PartB.java
@@ -1,6 +1,19 @@
 public ArrayList<Match> buildMatches() {
   ArrayList<Match> result = new ArrayList<Match>();
   int len = competitorList.size();
+  //New Plan
+  int temp = 0;
+  if (len % 2 == 0) {
+    temp = 1; 
+  }
+  for (int i = 0; i < len/2; i++) {
+    Competitors p1 = competitorList.get(i+temp);
+    Competitors p2 = competitorList.get(len-i-1);
+    Match m = new Match(p1, p2);
+    result.add(m);
+  }
+  
+  /* Orginal Plan
   if (len % 2 == 0) {
     for (int i = 0; i < len/2; i++) {
       Competitors p1 = competitorList.get(i);
@@ -17,5 +30,6 @@ public ArrayList<Match> buildMatches() {
       result.add(m);
     }
   }
+  */
   return result;
 }
